069bb42aca1b8ae3f71c94486f03a27f4dabda0f
mogamoga <mogamoga.g1thu6@gmail.com>
2023-12-09 23:03:37 +0900

    心がスキップ気づけばコネクト

--- main.js
+++ main.js
@@ -4,10 +4,28 @@ const context = canvas.getContext("2d");
 
 const image = new Image();
 
-image.onload = () => {
+image.onload = main;
+image.src = "images/clover_days.jpg";
+
+function main() {
     canvas.width = image.width;
     canvas.height = image.height;
     context.drawImage(image, 0, 0);
-};
+    const imageData = context.getImageData(0, 0, canvas.width, canvas.height).data;
+    const imageRgbData = toRgbData(imageData);
+}
 
-image.src = "images/clover_days.jpg";
+function toRgbData(data) {
+    const rgbData = [];
+    for (let i = 0; i < data.length; i += 4) {
+        const red   = data[i];
+        const green = data[i + 1];
+        const blue  = data[i + 2];
+        rgbData.push({
+            r: red, g: green, b: blue
+        });
+    }
+    return rgbData;
+}
+
+
