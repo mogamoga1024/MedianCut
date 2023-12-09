29bf9a32d861716aef6d24debedd9a6fd29e8ee1
mogamoga <mogamoga.g1thu6@gmail.com>
2023-12-09 22:43:50 +0900

    きらめいてピースはじまるよ

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 
 const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
 
+const image = new Image();
 
+image.onload = () => {
+    canvas.width = image.width;
+    canvas.height = image.height;
+    context.drawImage(image, 0, 0);
+};
+
+image.src = "images/clover_days.jpg";
